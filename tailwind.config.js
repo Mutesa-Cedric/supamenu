ae9f63ecbfce951154d9a823170aab8ceb9bf327
Mutesa-Cedric <mutesacedric@gmail.com>
2024-05-18 14:38:01 +0200

    setup : tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{tsx,ts,js,jsx}",
+    "./components/**/*.{tsx,ts,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
